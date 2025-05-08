598166f260db52a391b8fce0576cb4cbdf3e879e
Guilherme Pappi <guipappibr@gmail.com>
2025-05-07 22:29:39 -0300

    fix: Correção de importação para deploy

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 import styles from "../page.module.css";
-import Header from "@/components/Header/Header";
-import Main from "@/components/Main/page";
-import About from "../../components/About/About";
-import Works from "@/components/Works/works";
-import SectionCarousel from "@/components/SectionCarousel/SectionCarousel";
+import Header from "../../components/Header/Header";
+import Main from "../../components/Main/page";
+import About from "../../components/About/about";
+import Works from "../../components/Works/works";
+import SectionCarousel from "../../components/SectionCarousel/SectionCarousel";
 import { ToastContainer } from "react-toastify";
 
 export default function Home() {
