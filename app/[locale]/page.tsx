43ef21857cd1458ffdeab58b6cd83d538b36bff9
Guilherme Pappi <guipappibr@gmail.com>
2025-05-07 23:29:02 -0300

    fix: Corrigindo caminho para importar o componente Header na page.

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import styles from "../page.module.css";
-import Header from "../../components/Header/Header";
+import Header from "../../components/Header/header";
 import Main from "../../components/Main/page";
 import About from "../../components/About/about";
 import Works from "../../components/Works/works";
