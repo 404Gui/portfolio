56305a2b2b14c4cad15c097900e124eea2a56964
Guilherme Pappi <guipappibr@gmail.com>
2025-05-29 18:00:30 -0300

    refactor: ajustar imports para usar exports nomeados em vez de padrão

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,13 @@
-"use client"
+"use client";
 import styles from "../page.module.css";
-import Header from "../../components/Header/header";
-import Main from "../../components/Main/page";
-import About from "../../components/About/about";
-import Works from "../../components/Works/works";
-import SectionCarousel from "../../components/SectionCarousel/SectionCarousel";
+import { Header } from "../../components/Header/header";
+import { Main } from "../../components/Main/page";
+import { About } from "../../components/About/about";
+import { Works } from "../../components/Works/works";
+import { SectionCarousel } from "../../components/SectionCarousel/SectionCarousel";
 import { ToastContainer } from "react-toastify";
-import Footer from "../../components/Footer/footer";
+import { Footer } from "../../components/Footer/footer";
+import { getTranslations } from 'next-intl/server';
 
 export default function Home() {
   return (
