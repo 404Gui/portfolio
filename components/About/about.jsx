fadb95e78157747535805d55c9831b9b3d25c4ce
Guilherme Pappi <guipappibr@gmail.com>
2025-06-06 19:55:01 -0300

    feat: atualiza o texto do componente About para mais clareza

--- components/About/about.jsx
+++ components/About/about.jsx
@@ -12,20 +12,31 @@ export function About({ t }) {
     <div className={styles.about} id="about">
       <div className={styles.content}>
         <h1 className={styles.title}>{t("title")}</h1>
+
         <p className={styles.texto}>
           {t.rich("p1", {
             strong: (chunks) => <strong>{chunks}</strong>,
           })}
         </p>
-        <p className={styles.texto}>{t("p2")}</p>
+
         <p className={styles.texto}>
-          {t.rich("p3", {
+          {t.rich("p2", {
             span: (chunks) => <span className={styles.destaque}>{chunks}</span>,
           })}
         </p>
+
+        <p className={styles.texto}>{t("p3")}</p>
+
         <p className={styles.texto}>{t("p4")}</p>
+
         <p className={styles.texto}>
           {t.rich("p5", {
+            strong: (chunks) => <strong>{chunks}</strong>,
+          })}
+        </p>
+
+        <p className={styles.texto}>
+          {t.rich("p6", {
             strong: (chunks) => <strong>{chunks}</strong>,
           })}
         </p>
