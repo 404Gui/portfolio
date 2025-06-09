188b0e45808cfe05aeff7bba6e7a63d543135da0
Guilherme Pappi <guipappibr@gmail.com>
2025-06-09 17:58:20 -0300

    fix: correct import paths for Works and Footer components for consistency

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,19 +3,19 @@ import styles from "../page.module.css";
 import { Header } from "../../components/Header/header";
 import { Main } from "../../components/Main/page";
 import { About } from "../../components/About/about";
-import { Works } from "../../components/Works/Works";
+import { Works } from "../../components/Works/works"
 import { SectionCarousel } from "../../components/SectionCarousel/SectionCarousel";
 import { ToastContainer } from "react-toastify";
-import { Footer } from "../../components/Footer/Footer";
-import { getTranslations } from 'next-intl/server';
+import { Footer } from "../../components/Footer/footer";
+import { getTranslations } from "next-intl/server";
 
 export default async function Home() {
-  const tHeader = await getTranslations('header');
-  const tMain = await getTranslations('main');
-  const tAbout = await getTranslations('about');
-  const tWorks = await getTranslations('projects');
-  const tCarousel = await getTranslations('projects');
-  const tFooter = await getTranslations('footer');
+  const tHeader = await getTranslations("header");
+  const tMain = await getTranslations("main");
+  const tAbout = await getTranslations("about");
+  const tWorks = await getTranslations("projects");
+  const tCarousel = await getTranslations("projects");
+  const tFooter = await getTranslations("footer");
 
   return (
     <div className={styles.page}>
